e022ebf047a5457c58a58ab13edc04c4111236a9
MalteEbner <malte.ebner@gmail.com>
2021-05-04 14:24:48 +0200

    first draft of NNCLR

---
+++ source/nn_memory_bank.py
@@ -0,0 +1,17 @@
+import torch
+from lightly.loss.memory_bank import MemoryBankModule
+
+
+class NNmemoryBankModule(MemoryBankModule):
+    def __init__(self, size: int = 2 ** 16):
+        super(NNmemoryBankModule, self).__init__(size)
+
+    def forward(self,
+                output: torch.Tensor,
+                labels: torch.Tensor = None,
+                update: bool = False):
+        output, bank = super(NNmemoryBankModule, self).forward(output, labels, update)
+        similarity_matrix = torch.einsum("nd,md->nm", output, bank)
+        index_nearest_neighbours = torch.argmax(similarity_matrix, dim=1)
+        nearest_neighbours = torch.index_select(bank, dim=0, index=index_nearest_neighbours)
+        return nearest_neighbours, bank
